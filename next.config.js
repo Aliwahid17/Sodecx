b5a62e1c70ed2ab8bdef9f4c5d3318c91f0ee671
Wahid Ali <aliwahid1716@gmail.com>
2023-12-09 14:37:42 +0530

    fix : typo

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-        config.resolve.alias['@assets'] = path.join(__dirname, 'path/to/your/assets');
+        config.resolve.alias['@assets'] = path.join(__dirname, 'assets');
         return config;
     },
 }
