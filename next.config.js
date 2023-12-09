d8da077f17d5d5a5c342368f4b34b8487a7780d2
Wahid Ali <aliwahid1716@gmail.com>
2023-12-09 14:36:25 +0530

    fix:resolve webpack issue

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
+const path = require('path');
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+        config.resolve.alias['@assets'] = path.join(__dirname, 'path/to/your/assets');
+        return config;
+    },
+}
 
 module.exports = nextConfig
