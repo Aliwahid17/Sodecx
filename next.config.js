f58350182483b4e7b3007f5b4de89811a19e9039
Wahid Ali <aliwahid1716@gmail.com>
2023-12-16 14:33:28 +0530

    UPDAEte

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
-const path = require('path');
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-        config.resolve.alias['@assets'] = path.join(__dirname, 'assets');
-        return config;
-    },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
